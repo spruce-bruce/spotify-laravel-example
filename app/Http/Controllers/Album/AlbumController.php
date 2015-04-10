1e0ecf8a5adfecb0dd25c88ca000de68f386c85f
spruce-bruce <aarondbruce@gmail.com>
2015-04-09 20:59:26 -0700

    created my first laravel controller

---
+++ app/Http/Controllers/Album/AlbumController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SpotifyExample\Http\Controllers\Album;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+
+class AlbumController extends Controller
+{
+    public function getAlbums(Request $request) {
+        die("HERE WE ARE");
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('albums', 'Album\AlbumController@getAlbums');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
