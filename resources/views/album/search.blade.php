85f24978aa06b7ae531cd0ff48e10443e8e72cc0
spruce-bruce <aarondbruce@gmail.com>
2015-04-11 18:52:11 -0700

    added search view

---
+++ resources/views/album/search.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+
+    <form method="POST" action="">
+        <input type="text" name="query" id="query" />
+        <button>Search</button>
+    </form>
+
+@endsection
